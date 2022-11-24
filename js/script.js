a10dde7a52b51fa4ad81175c1cab5ee3a9d6aa28
Luiz Henrique <luizha879@gmail.com>
2022-11-24 13:12:47 -0300

    show numbers at the screen

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const previousOperationText = document.querySelector("#previous-operation");
+const currentOperationText = document.querySelector("#current-operation");
+const buttons = document.querySelectorAll("#buttons-container button");
+
+class Calculator {
+  constructor(previousOperationText, currentOperationText) {
+    this.previousOperationText = previousOperationText;
+    this.currentOperationText = currentOperationText;
+    this.currentOperation = "";
+  }
+  // add digit to calculator screen
+  addDigit(digit) {
+    // check if current operation already has a dot
+    if (digit === "." && this.currentOperationText.innerText.includes(".")) {
+      return;
+    }
+
+    this.currentOperation = digit;
+    this.updatescreen();
+  }
+
+  // Change values of the calc screen
+  updatescreen() {
+    this.currentOperationText.innerText += this.currentOperation;
+  }
+}
+
+const calc = new Calculator(previousOperationText, currentOperationText);
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const value = e.target.innerText;
+
+    if (+value >= 0 || value === ".") {
+      calc.addDigit(value);
+    } else {
+      console.log("op: " + value);
+    }
+  });
+});
